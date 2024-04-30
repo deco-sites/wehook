42696f1642ae6b4f16476d0e735fc3e57b662de7
João Paulo França <jpdotfranca@gmail.com>
2024-04-30 00:40:24 +0000

    Release Staging environment with 2 changes made by João Paulo França

--- sections/ItWorks.tsx
+++ sections/ItWorks.tsx
@@ -26,36 +26,7 @@ export default function Section({ name = "It Works!", count = 0 }: Props) {
     >
       <div class="leading-10 text-6xl" dangerouslySetInnerHTML={{
         __html: name,
-      }}/>
-
-      <div class="flex flex-col items-center justify-center gap-2">
-        <div class="flex items-center gap-4">
-          <button
-            hx-target="#it-works"
-            hx-swap="outerHTML"
-            hx-get={downLink["f-partial"]} // htmx link for this section with the down vote props
-            class="btn btn-sm btn-circle btn-outline no-animation"
-          >
-            <span class="inline [.htmx-request_&]:hidden">
-              -
-            </span>
-            <span class="loading loading-spinner hidden [.htmx-request_&]:inline" />
-          </button>
-          <span>{count}</span>
-          <button
-            hx-target="#it-works"
-            hx-swap="outerHTML"
-            hx-get={upLink["f-partial"]} // htmx link for this section with the up vote props
-            class="btn btn-sm btn-circle btn-outline no-animation"
-          >
-            <span class="inline [.htmx-request_&]:hidden">
-              +
-            </span>
-            <span class="loading loading-spinner hidden [.htmx-request_&]:inline" />
-          </button>
-        </div>
-        <div class="text-sm">Powered by HTMX</div>
-      </div>
+      }}/> 
     </div>
   );
 }
